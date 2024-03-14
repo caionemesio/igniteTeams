2ad99aa6dab8c1589942f87fa503bea8f6384ebf
Caio Nemesio <caionemesio2@gmail.com>
2024-03-14 18:52:44 -0300

    config environment and learning styled Components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from "react-native";
-
+import { Container, Title } from "./styles";
 export default function Groups() {
   return (
-    <View>
-      <Text>oi</Text>
-    </View>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
+
   )
 }
